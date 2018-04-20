533e16e25a4f08c7e01b86b977716c08f7bea77f
hawkeye116477 <hawkeye116477@gmail.com>
2018-04-20 23:45:57 +0200

    Update Travis config, scripts and other things [ci skip]

---
+++ scripts/TRAVIS_LETS_DO_THIS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ost_commit_katalog=$(dirname $(git diff-tree --no-commit-id --name-only -r master))
+if [ "$ost_commit_katalog" == "sections/adblock_social_filters" ]; then
+    katalog=adblock_social_filters
+    plik=adblock_social_filters.txt
+elif [ "$ost_commit_katalog" == "sections/cookies_filters" ]; then
+    katalog=cookies_filters
+    pik=cookies_filters.txt
+elif [ "$ost_commit_katalog" == "sections/cookies_uB_AG" ]; then
+    katalog=cookies_filters
+    plik=cookies_uB_AG.txt
+elif [ "$ost_commit_katalog" == "sections/social_filters_uB_AG" ]; then
+    katalog=adblock_social_filters
+    plik=social_filters_uB_AG.txt
+fi;
+
+
+./scripts/VICHS_Travis.sh $katalog/$plik
--- scripts/VICHS.sh
+++ scripts/VICHS.sh
@@ -7,16 +7,13 @@ for i in "$@"; do
     # FILTR to nazwa pliku, który chcemy zaktualizować
     FILTR=$(basename $i .txt)
 
-    # FILTR_SCIEZKA to ścieżka, w której znajduje się filtr
-    FILTR_SCIEZKA=$(basename $(dirname $i))
-
     # Sciezka to miejsce, w którym znajduje się skrypt
     sciezka=$(dirname "$0")
     
     TEMPLATE=$sciezka/../templates/${FILTR}.template
     KONCOWY=$i
     TYMCZASOWY=$sciezka/../${FILTR}.temp
-    SEKCJE_KAT=$sciezka/../sections/${FILTR_SCIEZKA}
+    SEKCJE_KAT=$sciezka/../sections/${FILTR}
     
 
     # Podmienianie zawartości pliku końcowego na zawartość template'u
