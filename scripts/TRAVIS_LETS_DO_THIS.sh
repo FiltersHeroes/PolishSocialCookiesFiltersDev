1b5d3d7c696f09b700c0ce2519a08ea6c1fc207f
hawkeye116477 <hawkeye116477@gmail.com>
2018-05-21 17:44:42 +0200

    Fix script

--- scripts/TRAVIS_LETS_DO_THIS.sh
+++ scripts/TRAVIS_LETS_DO_THIS.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ost_commit_katalog=$(dirname $(git diff-tree --no-commit-id --name-only -r master))
-if [ "$ost_commit_katalog" == "sections/adblock_social_filters" ]; then
+if [ "$ost_commit_katalog" == "sections/adblock_social_list" ]; then
     katalog=adblock_social_filters
     plik=adblock_social_list.txt
-elif [ "$ost_commit_katalog" == "sections/cookies_filters" ]; then
+elif [ "$ost_commit_katalog" == "sections/adblock_cookies" ]; then
     katalog=cookies_filters
     pik=adblock_cookies.txt
 elif [ "$ost_commit_katalog" == "sections/cookies_uB_AG" ]; then
