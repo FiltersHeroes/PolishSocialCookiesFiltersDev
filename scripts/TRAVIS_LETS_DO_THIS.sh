fd38aed2794d99bd11ed877e1fb007bd42167f6b
hawkeye116477 <hawkeye116477@gmail.com>
2019-01-02 21:28:29 +0100

    Let's tidy up :smile: [ci skip]

--- scripts/TRAVIS_LETS_DO_THIS.sh
+++ scripts/TRAVIS_LETS_DO_THIS.sh
@@ -7,13 +7,13 @@ if [ "$ost_commit_katalog" == "sections/adblock_social_list" ]; then
 elif [ "$ost_commit_katalog" == "sections/adblock_cookies" ]; then
     katalog=cookies_filters
     plik=adblock_cookies.txt
-elif [ "$ost_commit_katalog" == "sections/cookies_uB_AG" ]; then
+elif [ "$ost_commit_katalog" == "sections/adblock_cookies/uBO_AG" ]; then
     katalog=cookies_filters
     plik=cookies_uB_AG.txt
-elif [ "$ost_commit_katalog" == "sections/social_filters_uB_AG" ]; then
+elif [ "$ost_commit_katalog" == "sections/adblock_social_list/uBO_AG" ]; then
     katalog=adblock_social_filters
     plik=social_filters_uB_AG.txt
 fi;
 
 
-./scripts/VICHS_Travis.sh $katalog/$plik
+./scripts/VICHS.sh $katalog/$plik
--- scripts/VICHS_auto.sh
+++ scripts/VICHS_auto.sh
@@ -14,20 +14,20 @@ for i in $ost_zmieniony_plik; do
             lista+=" "cookies_filters/adblock_cookies.txt
         fi
     fi
-    
-    if [[ "$i" == "sections/cookies_uB_AG"* ]]; then
+
+    if [[ "$i" == "sections/adblock_cookies/uBO_AG"* ]]; then
         if [[ "$lista" != *" cookies_filters/cookies_uB_AG.txt"* ]] ;then
             lista+=" "cookies_filters/cookies_uB_AG.txt
         fi
     fi
-    
+
     if [[ "$i" == "sections/adblock_social_list"* ]]; then
         if [[ "$lista" != *" adblock_social_filters/adblock_social_list.txt"* ]] ;then
             lista+=" "adblock_social_filters/adblock_social_list.txt
         fi
     fi
-    
-    if [[ "$i" == "sections/social_filters_uB_AG"* ]]; then
+
+    if [[ "$i" == "sections/adblock_social_list/uBO_AG"* ]]; then
         if [[ "$lista" != *" adblock_social_filters/social_filters_uB_AG.txt"* ]] ;then
             lista+=" "adblock_social_filters/social_filters_uB_AG.txt
         fi
