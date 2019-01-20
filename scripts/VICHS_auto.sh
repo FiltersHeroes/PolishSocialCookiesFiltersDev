949369b844631a0be43dcb6d2d87e838e475a105
hawkeye116477 <hawkeye116477@gmail.com>
2019-01-20 11:37:07 +0100

    Update script and add CircleCI config [ci skip]

--- scripts/VICHS_auto.sh
+++ scripts/VICHS_auto.sh
@@ -5,10 +5,10 @@ sciezka=$(dirname "$0")
 
 cd $sciezka/..
 
-if [ "$CI" = "true" ] ; then
+if [ "$CI" = "true" ]; then
     git config --global user.email "PolishJarvis@int.pl"
     git config --global user.name "PolishJarvis"
-    ost_plik=$(git diff-tree --no-commit-id --name-only -r master)
+    ost_plik=$(git log --since="3 hours 58 minutes ago" --name-only --pretty=format: | sort | uniq)
 else
     ost_plik=$(git diff -z --name-only | xargs -0)
 fi
@@ -16,25 +16,25 @@ fi
 for i in $ost_plik; do
 
     if [[ "$i" == "sections/adblock_cookies/uBO_AG"* ]]; then
-        if [[ "$lista" != *" cookies_filters/cookies_uB_AG.txt"* ]] ;then
+        if [[ "$lista" != *" cookies_filters/cookies_uB_AG.txt"* ]]; then
             lista+=" "cookies_filters/cookies_uB_AG.txt
         fi
     fi
 
     if [[ "$i" == "sections/adblock_cookies"* ]]; then
-        if [[ "$lista" != *" cookies_filters/adblock_cookies.txt"* ]] ;then
+        if [[ "$lista" != *" cookies_filters/adblock_cookies.txt"* ]]; then
             lista+=" "cookies_filters/adblock_cookies.txt
         fi
     fi
 
     if [[ "$i" == "sections/adblock_social_list/uBO_AG"* ]]; then
-        if [[ "$lista" != *" adblock_social_filters/social_filters_uB_AG.txt"* ]] ;then
+        if [[ "$lista" != *" adblock_social_filters/social_filters_uB_AG.txt"* ]]; then
             lista+=" "adblock_social_filters/social_filters_uB_AG.txt
         fi
     fi
 
     if [[ "$i" == "sections/adblock_social_list"* ]]; then
-        if [[ "$lista" != *" adblock_social_filters/adblock_social_list.txt"* ]] ;then
+        if [[ "$lista" != *" adblock_social_filters/adblock_social_list.txt"* ]]; then
             lista+=" "adblock_social_filters/adblock_social_list.txt
         fi
     fi
